5c23452c814a194239fbcf08d89c7f9c6a141772
Brian DuSell <bdusell@gmail.com>
2020-02-11 03:30:37 -0500

    Make sure `route` and `available_methods` are always initialized.
    Initialize them before errors can occur.

--- src/Handlers/Configure.php
+++ src/Handlers/Configure.php
@@ -12,13 +12,14 @@ namespace Jitsu\App\Handlers;
 class Configure implements \Jitsu\App\Handler {
 
 	public function handle($data) {
+		$data->route = null;
+		$data->available_methods = array();
 		$config = Util::requireProp($data, 'config');
 		$route = $config->removePath($data->request->path());
+		$data->route = $route;
 		if($route === null) {
 			throw new \LogicException('misconfigured base path');
 		}
-		$data->route = $route;
-		$data->available_methods = array();
 		return false;
 	}
 }
